a4a3cf3b98704ca768f9c39c87e8a76cac5a5ca7
yash-patel268 <yashp2126@gmail.com>
2022-05-25 19:00:07 -0400

    Added logout method

--- server.js
+++ server.js
@@ -8,6 +8,7 @@ const bcrypt = require('bcrypt')
 const passport = require('passport')
 const flash = require('express-flash')
 const session = require('express-session')
+const methodOverride = require('method-override')
 
 const initializePassport = require('./passport-config')
 initializePassport(
@@ -28,6 +29,7 @@ app.use(session({
 }))
 app.use(passport.initialize())
 app.use(passport.session())
+app.use(methodOverride('_method'))
 
 app.get('/', checkAuthenticated, (req, res) => {
     res.render('index.ejs', {name: req.user.name})
@@ -61,6 +63,11 @@ app.post('/register', checkNotAuthenticated, async (req, res) => {
         res.redirect('/register')
     }
     console.log(users)
+})
+
+app.delete('/logout', (req, res) => {
+    req.logOut()
+    req.redirect('/login')
 })
 
 function checkAuthenticated(req, res, next) {
